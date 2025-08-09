59509f4510f65096d2a0e1b1fe224c147068593c
RisaDev <151885272+RisaDev@users.noreply.github.com>
2025-08-09 20:41:07 +0300

    API13

--- ECommonsLite/ImGuiMethods/ImGuiEx/ImGuiEx.cs
+++ ECommonsLite/ImGuiMethods/ImGuiEx/ImGuiEx.cs
@@ -1,4 +1,4 @@
-﻿using ImGuiNET;
+﻿using Dalamud.Bindings.ImGui;
 using System;
 using System.Numerics;
 using System.Runtime.InteropServices;
@@ -15,76 +15,36 @@ public static unsafe partial class ImGuiEx
     public static void EzTabBar(string id, string KoFiTransparent, string openTabName, params (string name, Action function, Vector4? color, bool child)[] tabs) => EzTabBar(id, KoFiTransparent, openTabName, ImGuiTabBarFlags.None, tabs);
     public static void EzTabBar(string id, string KoFiTransparent, string openTabName, ImGuiTabBarFlags flags, params (string name, Action function, Vector4? color, bool child)[] tabs)
     {
-        ImGui.BeginTabBar(id, flags);
-        foreach (var x in tabs)
+        if (ImGui.BeginTabBar(id, flags))
         {
-            if (x.name == null) continue;
-            if (x.color != null)
+            foreach (var x in tabs)
             {
-                ImGui.PushStyleColor(ImGuiCol.Text, x.color.Value);
-            }
-            if (ImGuiEx.BeginTabItem(x.name, openTabName == x.name ? ImGuiTabItemFlags.SetSelected : ImGuiTabItemFlags.None))
-            {
+                if (x.name == null) continue;
                 if (x.color != null)
                 {
-                    ImGui.PopStyleColor();
+                    ImGui.PushStyleColor(ImGuiCol.Text, x.color.Value);
                 }
-                if (x.child) ImGui.BeginChild(x.name + "child");
-                x.function();
-                if (x.child) ImGui.EndChild();
-                ImGui.EndTabItem();
-            }
-            else
-            {
-                if (x.color != null)
+                if (ImGui.BeginTabItem(x.name, openTabName == x.name ? ImGuiTabItemFlags.SetSelected : ImGuiTabItemFlags.None))
                 {
-                    ImGui.PopStyleColor();
+                    if (x.color != null)
+                    {
+                        ImGui.PopStyleColor();
+                    }
+                    if (x.child) ImGui.BeginChild(x.name + "child");
+                    x.function();
+                    if (x.child) ImGui.EndChild();
+                    ImGui.EndTabItem();
+                }
+                else
+                {
+                    if (x.color != null)
+                    {
+                        ImGui.PopStyleColor();
+                    }
                 }
             }
-        }
-        //if (KoFiTransparent != null) PatreonBanner.RightTransparentTab();
-        ImGui.EndTabBar();
-    }
-
-    public static unsafe bool BeginTabItem(string label, ImGuiTabItemFlags flags)
-    {
-        var num = 0;
-        byte* ptr;
-        if (label != null)
-        {
-            num = Encoding.UTF8.GetByteCount(label);
-            ptr = Allocate(num + 1);
-            var utf = GetUtf8(label, ptr, num);
-            ptr[utf] = 0;
-        }
-        else
-        {
-            ptr = null;
-        }
-
-        byte* p_open2 = null;
-        var num2 = ImGuiNative.igBeginTabItem(ptr, p_open2, flags);
-        if (num > 2048)
-        {
-            Free(ptr);
-        }
-        return num2 != 0;
-    }
-    internal static unsafe byte* Allocate(int byteCount)
-    {
-        return (byte*)(void*)Marshal.AllocHGlobal(byteCount);
-    }
-
-    internal static unsafe void Free(byte* ptr)
-    {
-        Marshal.FreeHGlobal((IntPtr)ptr);
-    }
-
-    internal static unsafe int GetUtf8(string s, byte* utf8Bytes, int utf8ByteCount)
-    {
-        fixed (char* chars = s)
-        {
-            return Encoding.UTF8.GetBytes(chars, s.Length, utf8Bytes, utf8ByteCount);
+            //if (KoFiTransparent != null) PatreonBanner.RightTransparentTab();
+            ImGui.EndTabBar();
         }
     }
 }
